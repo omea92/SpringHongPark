c47977f6d9b1e683d96ecdb25f023f3d4dac8c7b
omea92 <thdjun92@naver.com>
2024-05-15 16:01:08 +0900

    홍팍스프링부트3 롬복 SELF CHECK

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -4,13 +4,16 @@ import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
+
     @Autowired
     private MemberRepository memberRepository;
 
@@ -21,15 +24,16 @@ public class MemberController {
 
     @PostMapping("/join") //form - action 경로와 반드시 일치
     public String createArticle(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
 
         //DTO -> Entity
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
 
         //Repository -> Entity DB SAVE
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+
         return "";
     }
 }
